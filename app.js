e16684164b41fb9cc71cdc58c770862a50046e3f
Ahmaad2001 <ahmadmamounnaqawa@hotmail.com>
2023-06-15 19:22:35 +0300

    Done

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const PORT = 8000;
 const express = require("express");
 const app = express();
-const router = require("./api/accounts/accounts.routes");
+const accountsRouter = require("./api/accounts/accounts.routes");
 
 app.use(express.json());
-app.use("/api/accounts", router);
+app.use("/api/accounts", accountsRouter);
 
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
